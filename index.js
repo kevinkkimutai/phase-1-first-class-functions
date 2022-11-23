75a5fd380b697174c58de9d9d2179d4031a990c3
kevinkkimutai <kiruikevinkim@gmail.com>
2022-11-23 09:27:28 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+// Function call back
+function receivesAFunction(callback) {
+    return callback();
+  }
+  receivesAFunction(() => {
+
+  });
+  // named function
+  function returnsANamedFunction() {
+    let sam = () => console.log("mans")
+  return (sam)
+  }
+
+// anonymous function
+var returnsAnAnonymousFunction = function() {
+    return (() => {
+alert('This an anonymous function')
+    } 
+    )
+}
